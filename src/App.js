a9bf4b4a6c28ecaf166d81dc68d0933d23f1a6bc
matt holzel <matt@holzel.com>
2018-11-12 21:09:24 +0100

    added a lightbox

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import Hammer from 'hammerjs';
 import './App.css';
 import Sidebar from './sidebar/Sidebar'
 import Buttons from './buttons/Buttons'
+import Lightbox from 'react-image-lightbox';
+import 'react-image-lightbox/style.css'; // This only needs to be imported once in your app
 
-// Loading indicator
+// Loading indicator needs some styling
 // Maintain state when selecting images.
 // Infinite scrolling
 // Non-uniform image sizes 
@@ -13,6 +16,12 @@ const imageURL = 'http://codekafana.com/family/wp-content/uploads/';
 const baseURL = true ? 'http://codekafana.com/react3/' : 'http://localhost/';
 const size = '150x150';
 const sizeEnding = size + '.jpg';
+function urlGenerator(shortUrl) {
+  return {
+    smallUrl: imageURL + shortUrl,
+    fullsizeUrl: imageURL + shortUrl.slice(0, -1 - sizeEnding.length) + '.jpg'
+  };
+}
 
 class App extends Component {
 
@@ -20,10 +29,23 @@ class App extends Component {
     urls: [],
     scale: 25,
     showSidebar: true,
+    isLoading: true,
+    lightboxIndex: -1,
   };
 
+  onTouchStart = event => {
+    if (event.touches.length >= 2) {
+      this.hammer.get("pinch").set({ enable: true });
+    }
+  }
+  onTouchEnd = event => {
+    if (event.touches.length < 2) {
+      this.hammer.get("pinch").set({ enable: false });
+    }
+  }
+
   onDateSelected = date => {
-    this.setState({ showSidebar: false });
+    this.setState({ showSidebar: false, isLoading: true });
     const month = months[date.getUTCMonth()];
     const year = date.getUTCFullYear();
     const url = baseURL + 'files.php?m=' + month + '&y=' + year;
@@ -34,7 +56,7 @@ class App extends Component {
         return data.json();
       })
       .then(data => this.selectSize(data))
-      .then(data => this.setState({ urls: data }));
+      .then(data => this.setState({ urls: data, isLoading: false }));
   }
 
   onCancelSidebar = () => {
@@ -45,14 +67,19 @@ class App extends Component {
     this.setState(state => ({ showSidebar: !state.showSidebar }));
   }
 
+  imageSelected = index => {
+    this.setState({ lightboxIndex: index });
+  }
+
   render() {
+    const urls = this.state.urls;
+    const lightboxIndex = this.state.lightboxIndex;
     const style = { width: this.state.scale.toString() + '%' };
     return (<div>
-      <div className="grid">
-        {this.state.urls.map(entry => {
-          const fullSizeUrl = imageURL + entry.slice(0, -1 - sizeEnding.length) + '.jpg'
-          return (<div className="cell" key={fullSizeUrl} style={style}>
-            <a href={fullSizeUrl}><img src={imageURL + entry} className="responsive-image" alt="" /></a>
+      <div className="grid" ref={elem => this.gridElement = elem} onTouchStart={this.onTouchStart} onTouchEnd={this.onTouchEnd}>
+        {urls.map((entry, index) => {
+          return (<div className="cell" key={entry.fullsizeUrl} style={style}>
+            <img src={entry.smallUrl} className="responsive-image" alt="" onClick={() => this.imageSelected(index)} />
           </div>)
         })
         }
@@ -60,14 +87,33 @@ class App extends Component {
       <Sidebar onDateSelected={this.onDateSelected}
         onCancelSidebar={this.onCancelSidebar}
         showSidebar={this.state.showSidebar} />
-      <Buttons zoomIn={this.zoomIn} zoomOut={this.zoomOut} toggleCalendar={this.toggleCalendar} />
+      <Buttons zoomIn={this.zoomIn} zoomOut={this.zoomOut} toggleCalendar={this.toggleCalendar} isLoading={this.state.isLoading} />
+      {(lightboxIndex >= 0) &&
+        <Lightbox
+          mainSrc={urls[lightboxIndex].fullsizeUrl}
+          nextSrc={urls[(lightboxIndex + 1) % urls.length].fullsizeUrl}
+          prevSrc={urls[(lightboxIndex + urls.length - 1) % urls.length].fullsizeUrl}
+          onCloseRequest={() => this.setState({ lightboxIndex: -1 })}
+          onMovePrevRequest={() =>
+            this.setState({
+              lightboxIndex: (lightboxIndex + urls.length - 1) % urls.length,
+            })
+          }
+          onMoveNextRequest={() =>
+            this.setState({
+              lightboxIndex: (lightboxIndex + 1) % urls.length,
+            })
+          }
+        />
+      }
+
     </div>);
   }
 
   selectSize = urls => {
     return urls.reduce(function (correctSizeUrls, url) {
       if (url.endsWith(sizeEnding)) {
-        correctSizeUrls.push(url);
+        correctSizeUrls.push(urlGenerator(url));
       }
       return correctSizeUrls;
     }, []);
@@ -81,7 +127,16 @@ class App extends Component {
     this.setState(state => ({ scale: state.scale > 6.26 ? state.scale / 2 : state.scale }));
   }
 
+  pinchend = event => {
+    if (event.scale > 1.25)
+      this.zoomIn();
+    else if (event.scale < .75)
+      this.zoomOut();
+  }
+
   componentDidMount() {
+    this.hammer = Hammer(this.gridElement);
+    this.hammer.on('pinchend', this.pinchend);
     this.onDateSelected(new Date());
   }
 }
--- src/buttons/Buttons.js
+++ src/buttons/Buttons.js
@@ -3,6 +3,7 @@ import DateRangeIcon from '@material-ui/icons/DateRange';
 import AddIcon from '@material-ui/icons/Add';
 import RemoveIcon from '@material-ui/icons/Remove';
 import Button from '@material-ui/core/Button';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import './Buttons.css'
 
 import { withStyles } from '@material-ui/core/styles';
@@ -18,9 +19,13 @@ class Buttons extends Component {
     render() {
         return (
             <div id="buttons" style={{ visibility: "visible" }}>
-                <Button variant="fab" color="primary" onClick={this.props.toggleCalendar} style={{ display: "block" }}>
-                    <DateRangeIcon fontSize="large" />
-                </Button>
+                <div>
+                    <Button variant="fab" color="primary" onClick={this.props.toggleCalendar} style={{ display: "block" }} disabled={this.props.isLoading}>
+                        {this.props.isLoading ? 
+                        <CircularProgress color="secondary" /> :
+                            <DateRangeIcon fontSize="large" />}
+                    </Button>
+                </div>
                 <Button variant="fab" color="primary" onClick={this.props.zoomIn} style={{ display: "block" }}>
                     <AddIcon fontSize="large" />
                 </Button>
